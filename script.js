ab46b64e05acecf43ec905299ab9bdcd93abceb3
gabrield14s <126288308+gabrield14s@users.noreply.github.com>
2023-08-29 14:59:36 +0000

    project-final v1.0

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const cityInput = document.querySelector("#city-input");
 const searchButton = document.querySelector("#search-button");
 
 let tagNameCity = document.getElementById("name-city");
+let tagCountryIcon = document.getElementById("country-icon");
 let tagDegress = document.getElementById("degress");
 let tagCloudDescription = document.getElementById("cloud-description");
+let descriptionIcon = document.getElementById("description-icon");
+let tagHumidity = document.getElementById("humidity");
+let tagWind = document.getElementById("wind");
 let divBoxWithWheatherData = document.getElementsByName("div-box-wheater");
 let divMenssageError = document.getElementById("div-menssage-error");
 let cityData = document.getElementById('city-data');
@@ -25,43 +29,47 @@ const getWeatherData = async (city) => {
 const showWeatherData = async () => {
   
   const cityValue = cityInput.value;
+
   if (cityValue) {
     try {
       const weatherData = await getWeatherData(cityValue);
-      let name = weatherData.name
+      const countryIcon = `https://flagsapi.com/${weatherData.sys.country}/flat/32.png`;
+      let name = weatherData.name;
       let temp =  weatherData.main.temp;
       let cloudDescription = weatherData.weather[0].description;
       
+      // Inserindo os dados obtidos do JSON em nosso HTML
+      tagNameCity.innerText = name;
+      tagDegress.innerText = parseInt(temp) + " C°";
+      tagCountryIcon.setAttribute("src", countryIcon);
+      descriptionIcon.setAttribute("src", `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`);
+      tagHumidity.innerText = `${weatherData.main.humidity}%`;
+      tagWind.innerText = `${weatherData.wind.speed}km/h`;
+      tagCloudDescription.innerText = cloudDescription;
+
       console.log(name);     
       console.log(temp);
       
       if (divMenssageError.style.display == "block"){
         divMenssageError.style.display = "none";
 
-        // Inserindo os dados obtidos do JSON em nosso HTML
-        tagNameCity.innerText = name;
-        tagDegress.innerText = temp + " C°";
-        tagCloudDescription.innerText = cloudDescription;
-
-        divBoxWithWheatherData[0].style.display = "block";
-        divBoxWithWheatherData[1].style.display = "block";
-        divBoxWithWheatherData[2].style.display = "block";
+        divBoxWithWheatherData[0].style.display = "flex";
+        divBoxWithWheatherData[1].style.display = "flex";
+        divBoxWithWheatherData[2].style.display = "flex";
+        divBoxWithWheatherData[3].style.display = "flex";
       }
 
-      // Inserindo os dados obtidos do JSON em nosso HTML
-      tagNameCity.innerText = name;
-      tagDegress.innerText = temp + " C°";
-      tagCloudDescription.innerText = cloudDescription;
-
-      divBoxWithWheatherData[0].style.display = "block";
-      divBoxWithWheatherData[1].style.display = "block";
-      divBoxWithWheatherData[2].style.display = "block";
+      divBoxWithWheatherData[0].style.display = "flex";
+      divBoxWithWheatherData[1].style.display = "flex";
+      divBoxWithWheatherData[2].style.display = "flex";
+      divBoxWithWheatherData[3].style.display = "flex";
       
     } catch (error) {
-      if (divBoxWithWheatherData[0].style.display == "block" & divBoxWithWheatherData[1].style.display == "block"){
+      if (divBoxWithWheatherData[0].style.display == "flex" & divBoxWithWheatherData[1].style.display == "flex" && divBoxWithWheatherData[2].style.display == "flex"){
         divBoxWithWheatherData[0].style.display = "none";
         divBoxWithWheatherData[1].style.display = "none";
         divBoxWithWheatherData[2].style.display = "none";
+        divBoxWithWheatherData[3].style.display = "none";
         divMenssageError.style.display = "block";
       }
       divMenssageError.style.display = "block";
